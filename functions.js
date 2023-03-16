24ef36d63045af73643e1344c0d8f06bd932d855
root <root@LEV>
2023-03-16 22:58:15 +0800

    feat: function for adding an account (excluding saving to user.txt) is added

--- functions.js
+++ functions.js
@@ -2,10 +2,45 @@
 // BSCS - CMSC 100 UV-7L
 
 import { v4 as uuidv4 } from 'uuid';
+import isEmail from 'validator/lib/isEmail.js';
 
+// function for generating user's unique id
 const generateUniqueID = (fname, lname) => {
     var id = fname.charAt(0) + lname + uuidv4().slice(0,8);
     return id.toLowerCase();
 }
 
-export {generateUniqueID}
+const addAccount = (account_details) => {
+    var flag = true;
+    // console.log(account_details)
+
+    // there should be 4 elements in the array
+    if (account_details.length != 4){
+        console.log(" ~ All fields should be present");
+        flag = false;
+    }
+    
+    // checks each input parameter if it is an empty string
+    const param_string = ["First name", "Last name", "Email"];
+    for (let i = 0; i<account_details.length-1; i++){
+        if(account_details[i] === ""){
+            console.log(" ~ " + param_string[i] + " should be present");
+            flag = false;
+        }
+    }
+
+    // checks if email is valid
+    if (!isEmail(account_details[2])){
+        console.log(" ~ Email should be in valid format (e.g. ada@love.com)");
+        flag = false;
+    }
+
+    // checks if age is at least 18
+    if (account_details[3] < 18){
+        console.log(" ~ Age should be at least 18");
+        flag = false;
+    }
+}
+
+
+export { generateUniqueID, addAccount }
--- test.js
+++ test.js
@@ -1,6 +1,28 @@
 // Olipas, Karl Kenneth Owen
 // BSCS - CMSC 100 UV-7L
 
-import { generateUniqueID } from "./functions.js"
+import { generateUniqueID, addAccount } from "./functions.js"
 
+// Test case for generating unique id
+console.log("\nGENERATING UNIQUE ID")
 console.log(generateUniqueID("Alan", "Turing"));
+
+// Test case incomplete number of fields
+console.log("\nINCOMPLETE FIELD")
+addAccount(["Alan", "Turing", "aturing@w3c.com",]);
+
+// Test case for empty input
+console.log("\nEMPTY FIELD")
+addAccount(["Alan", "", "aturing@w3c.com", 58]);
+
+// Test case for invalid email format
+console.log("\nINVALID EMAIL")
+addAccount(["Alan", "Turing", "aturingw3c.com", 58]);
+
+// Test case for age that is below 18
+console.log("\nAGE BELOW 18")
+addAccount(["Alan", "Turing", "aturing@w3c.com", 17]);
+
+// Test case for multiple invalid input
+console.log("\nMULTIPLE INVALID INPUT")
+addAccount(["", "Turing", "aturingw3c.com", 9]);
